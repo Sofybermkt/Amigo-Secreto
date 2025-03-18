bf748799ff0413faa47707e7305953909d777516
Sofybermkt <berburan@outlook.com>
2025-03-18 19:44:23 -0300

    app.js

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-alert('Amigo Secreto');
-let nombreusuario = Prompt("Me indica el nombre de su amigo:");
+//variables
+let nombreAmigo = Maria;
+let nombreUsuario = Prompt("Me indica el nombre de su amigo que comienza con M:");
+
+Console.log(nombreUsuario);
+/*
+Este codigo realiza 
+la comparacion de nombre de amigos posibles
+*/
+if (nombreUsuario == nombreAmigo) {
+  //Acertamos, fur verdadera la condicion
+alert('Acertaste el nombre es: ${nombreAmigo}');
+} else {
+//La condicion no se cumplio
+  alert('Lo siento, no acertaste el nombre´)
+        
