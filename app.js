8f3fb6b85e4082da817bdd572b5da17f99f13148
Sofybermkt <berburan@outlook.com>
2025-03-18 19:19:23 -0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+alert('Amigo Secreto');
+let nombreusuario = Prompt("Me indica el nombre de su amigo:");
